79da324ad6c878d1b10ba432c837d9831a3873c6
Hamza Amirni <hamzaamirni451@gmail.com>
2025-11-17 01:48:48 +0100

    chore: publish all local changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { AdProvider } from './contexts/AdContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AdProvider>
+      <App />
+    </AdProvider>
   </StrictMode>,
 );
