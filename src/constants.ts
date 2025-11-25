f23b36c04775eb3bdc739413eb895329fdd094dc
Hamza Amrni <hamzaamirni451@gmail.com>
2025-11-25 23:23:41 +0100

    Removed bot numbers, kept only personal contact and 2 WhatsApp channels

--- src/constants.ts
+++ src/constants.ts
@@ -44,10 +44,7 @@ export const myApps = [
 export const whatsappChannels = [
     { name: 'تواصل معي مباشرة', url: 'https://api.whatsapp.com/send/?phone=212708869993&text&type=phone_number&app_absent=0', IconComponent: SiWhatsapp },
     { name: 'قناة تحديثات البرمجيات', url: 'https://whatsapp.com/channel/0029ValXRoHCnA7yKopcrn1p', IconComponent: SiWhatsapp },
-    { name: 'قناة تحديثات القرآنيات', url: 'https://whatsapp.com/channel/0029VaDbxoYJpe8byewMhd3s', IconComponent: FaQuran },
-    { name: 'بوت الدعم التقني', url: 'https://api.whatsapp.com/send/?phone=18772241042&text&type=phone_number&app_absent=0', IconComponent: SiWhatsapp },
-    { name: 'بوت خدمة العملاء', url: 'https://api.whatsapp.com/send/?phone=18334363285&text&type=phone_number&app_absent=0', IconComponent: SiWhatsapp },
-    { name: 'بوت الاستفسارات', url: 'https://api.whatsapp.com/send/?phone=18002428478&text&type=phone_number&app_absent=0', IconComponent: SiWhatsapp }
+    { name: 'قناة تحديثات القرآنيات', url: 'https://whatsapp.com/channel/0029VaDbxoYJpe8byewMhd3s', IconComponent: FaQuran }
 ];
 
 // NOTE: Use sidebarProfile, skillsSection, journeySection, philosophySection from i18n.ts for translations
